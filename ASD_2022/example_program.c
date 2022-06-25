6b34223155e3ea4ad47e5bf4f2aa3bb72664971b
vankata05 <101401373+vankata05@users.noreply.github.com>
2022-06-25 16:11:30 +0300

    Did variant1 of the previous test.

--- ASD_2022/example_program.c
+++ ASD_2022/example_program.c
@@ -1,5 +1,14 @@
 //TODO:https://docs.google.com/document/d/1Vaja_UDG1fUM6sG8BNsV8J5d5k2XaB0CZqa78J4soXI/edit
 #include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+typedef struct bicycle_t {
+  char* brand;
+  int type;
+  float wheelSize;
+  bool diskStoppers
+} bicycle_t;
 
 int main(){
   
---
+++ ASD_2022/variant1.c
@@ -0,0 +1,73 @@
+//TODO:https://docs.google.com/document/d/1Xfk5gqXyPoLp2AaQd8c4C7B-iXnu0EPMxfoiHQDoy94/edit
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct warrior_t{
+    float damage, defence, health;
+    char name[32];
+} warrior_t;
+
+//randomly selects wich warrior to start the fight
+warrior_t row_a_dice(struct warrior_t first, struct warrior_t second){
+    int r = rand();
+    if(r % 2 == 0){
+        return first;
+    }else{
+        return second;
+    }
+}
+
+//implementation of the fight
+void fight(warrior_t first, warrior_t second){
+    if(strcmp(first.name, row_a_dice(first, second).name)){
+        while(first.health > 0 && second.health > 0){
+            printf("%s %.1fhp : %s %.1fhp\n", first.name, first.health, second.name, second.health);
+            printf("%s attacks\n", first.name);
+            if(second.defence >= first.damage){
+                second.health-=1;
+            }else{
+                second.health+=second.defence-first.damage;
+            }
+            printf("%s %.1fhp : %s %.1fhp\n", first.name, first.health, second.name, second.health);
+            printf("%s attacks\n", second.name);
+            if(first.defence >= second.damage){
+                first.health-=1;
+            }else{
+                first.health+=first.defence-second.damage;
+            }
+        }
+        if(first.health <= 0){
+            printf("%s wins", second.name);
+        }else{
+            printf("%s wins", first.name);
+        }
+    }
+}
+
+int main(){
+    warrior_t first;
+    warrior_t second;
+
+    printf("Enter the name of the first warrior:");
+    scanf("%s", &first.name);
+    printf("Enter the damage of the first warrior:");
+    scanf("%f", &first.damage);
+    printf("Enter the defence of the first warrior:");
+    scanf("%f", &first.defence);
+    printf("Enter the health of the first warrior:");
+    scanf("%f", &first.health);
+
+    printf("Enter the name of the second warrior:");
+    scanf("%s", &second.name);
+    printf("Enter the damage of the second warrior:");
+    scanf("%f", &second.damage);
+    printf("Enter the defence of the second warrior:");
+    scanf("%f", &second.defence);
+    printf("Enter the health of the second warrior:");
+    scanf("%f", &second.health);
+
+    fight(first, second);
+
+    return 0;
+}
