e0ad4b76f3cfbba16c6100635b5c3b0f060054c9
vankata05 <101401373+vankata05@users.noreply.github.com>
2022-06-25 13:18:16 +0300

    Added dynamic memory allocation.

--- ASD_2022/example_program.c
+++ ASD_2022/example_program.c
@@ -1,4 +1,4 @@
-//https://docs.google.com/document/d/1Vaja_UDG1fUM6sG8BNsV8J5d5k2XaB0CZqa78J4soXI/edit
+//TODO:https://docs.google.com/document/d/1Vaja_UDG1fUM6sG8BNsV8J5d5k2XaB0CZqa78J4soXI/edit
 #include<stdio.h>
 
 int main(){
