db9b1dd8ea9ebed91b76491c24bd7120bccb7545
vankata05 <vanio3768@abv.bg>
2023-06-13 10:42:53 -0700

    Update

--- SUBD/proekt/subd.c
+++ SUBD/proekt/subd.c
@@ -18,12 +18,15 @@ int createDB(){
         return 1;
     }
 
-    char *sql = "CREATE TABLE IF NOT EXISTS Orders("
-        "Id INTEGER PRIMARY KEY, Name VARCHAR(255), Quantity INT, Type VARCHAR(1), Price FLOAT, Date DATE"
-        ");"
-        "CREATE TABLE IF NOT EXISTS Todo("
-        "Id INTEGER REFERENCES Orders(Id)"
-        ");";
+char* sql = "CREATE TABLE IF NOT EXISTS Orders("
+    "Id INTEGER PRIMARY KEY, Name VARCHAR(255), Quantity INT, Type VARCHAR(1), Price FLOAT, Date TIMESTAMP, CustomerId INTEGER"
+    ");"
+    "CREATE TABLE IF NOT EXISTS Customers("
+    "Id INTEGER PRIMARY KEY, Name VARCHAR(255), Email VARCHAR(255), Phone VARCHAR(20)"
+    ");"
+    "CREATE TABLE IF NOT EXISTS Todo("
+    "Id INTEGER REFERENCES Orders(Id)"
+    ");";
 
     // printf("\n%s\n", sql);
 
@@ -76,7 +79,7 @@ int insertTodo(){
     return 0;
 }
 
-int insertOrder(char* name, int quantity, char* type, float price, char *date){
+int insertOrder(char* name, int quantity, char* type, float price, char* date, int customerId) {
     sqlite3* db;
     
     int rc = sqlite3_open("Biznis.db", &db);
@@ -86,11 +89,14 @@ int insertOrder(char* name, int quantity, char* type, float price, char *date){
         sqlite3_close(db);
         return 1;
     }
+
+    // ...
+
     char* sql;
     if(type[0] == '-'){
-        sql = "INSERT INTO Orders VALUES(NULL, ?, ?, ?, -?, ?);";
+        sql = "INSERT INTO Orders VALUES(NULL, ?, ?, ?, -?, ?, ?);";
     }else if(type[0] == '+'){
-        sql = "INSERT INTO Orders VALUES(NULL, ?, ?, ?, ?, ?);";
+        sql = "INSERT INTO Orders VALUES(NULL, ?, ?, ?, ?, ?, ?);";
     }else{
         fprintf(stderr, "Type must be '+' or '-'");
         return 1;
@@ -108,6 +114,7 @@ int insertOrder(char* name, int quantity, char* type, float price, char *date){
     sqlite3_bind_text(stmt, 3, type, -1, SQLITE_STATIC);
     sqlite3_bind_double(stmt, 4, price);
     sqlite3_bind_text(stmt, 5, date, -1, SQLITE_STATIC);
+    sqlite3_bind_int(stmt, 6, customerId);
 
     rc = sqlite3_step(stmt);
     if (rc != SQLITE_DONE) {
@@ -561,15 +568,234 @@ int printOrder(int id){
     return 0;
 }
 
-int main(){
+int addCustomer(char* name, char* email, char* phone) {
+    sqlite3* db;
+    
+    int rc = sqlite3_open("Biznis.db", &db);
+
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Cannot open database: %s", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return 1;
+    }
+
+    char* sql = "INSERT INTO Customers (Name, Email, Phone) VALUES (?, ?, ?);";
+
+    sqlite3_stmt *stmt;
+    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Failed to prepare statement: %s", sqlite3_errmsg(db));
+        return 1;
+    }
+
+    sqlite3_bind_text(stmt, 1, name, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 2, email, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 3, phone, -1, SQLITE_STATIC);
+
+    rc = sqlite3_step(stmt);
+    if (rc != SQLITE_DONE) {
+        fprintf(stderr, "Failed to insert data: %s", sqlite3_errmsg(db));
+        return 1;
+    }
+
+    sqlite3_finalize(stmt);
+    sqlite3_close(db);
+
+    return 0;
+}
+
+int deleteCustomer(int id) {
+    sqlite3* db;
+    
+    int rc = sqlite3_open("Biznis.db", &db);
+    
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Cannot open database: %s", sqlite3_errmsg(db));
+        sqlite3_close(db);
+
+        return 1;
+    }
+
+    char* sql = "DELETE FROM Customers WHERE Id = ?;";
+
+    sqlite3_stmt *stmt;
+    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+    
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Failed to prepare statement: %s", sqlite3_errmsg(db));
+
+        return 1;
+    }
+
+    sqlite3_bind_int(stmt, 1, id);
+
+    rc = sqlite3_step(stmt);
+    
+    if (rc != SQLITE_DONE) {
+        fprintf(stderr, "Failed to insert data: %s", sqlite3_errmsg(db));
+
+        return 1;
+    }
+
+    sqlite3_finalize(stmt);
+    sqlite3_close(db);
+
+    return 0;
+}
+
+int getCustomerOrders(int customerId) {
+    sqlite3* db;
+    
+    int rc = sqlite3_open("Biznis.db", &db);
+
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Cannot open database: %s", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return 1;
+    }
+
+    char* sql = "SELECT * FROM Orders WHERE CustomerId = ?;";
+
+    sqlite3_stmt *stmt;
+    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Failed to prepare statement: %s", sqlite3_errmsg(db));
+        return 1;
+    }
+
+    sqlite3_bind_int(stmt, 1, customerId);
+
+    rc = sqlite3_step(stmt);
+    if (rc != SQLITE_ROW) {
+        fprintf(stderr, "Failed to insert data: %s", sqlite3_errmsg(db));
+        return 1;
+    }
+
+    printf("| Id\t|Name\t|Quantity\t|Type\t|Price  |Date\t   |\n|----------------------------------------------------------|\n");
+    while (rc == SQLITE_ROW) {
+        printf("| %d\t|%s\t|%d\t\t|%s\t|%d\t|%s|\n", sqlite3_column_int(stmt, 0),
+               sqlite3_column_text(stmt, 1), sqlite3_column_int(stmt, 2),
+               sqlite3_column_text(stmt, 3), sqlite3_column_int(stmt, 4),
+               sqlite3_column_text(stmt, 5));
+
+        rc = sqlite3_step(stmt);
+    }
+
+    sqlite3_finalize(stmt);
+    sqlite3_close(db);
+
+    return 0;
+}
+
+int getCustomerOrdersByDate(int customerId, char* date) {
+    sqlite3* db;
+    
+    int rc = sqlite3_open("Biznis.db", &db);
+    
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Cannot open database: %s", sqlite3_errmsg(db));
+        sqlite3_close(db);
+
+        return 1;
+    }
+
+    char* sql = "SELECT * FROM Orders WHERE CustomerId = ? AND Date = ?;";
+
+    sqlite3_stmt *stmt;
+    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+    
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Failed to prepare statement: %s", sqlite3_errmsg(db));
+
+        return 1;
+    }
+
+    sqlite3_bind_int(stmt, 1, customerId);
+    sqlite3_bind_text(stmt, 2, date, -1, SQLITE_STATIC);
+
+    rc = sqlite3_step(stmt);
+    
+    if (rc != SQLITE_ROW) {
+        fprintf(stderr, "Failed to insert data: %s", sqlite3_errmsg(db));
+
+        return 1;
+    }
+
+    printf("| Id\t|Name\t|Quantity\t|Type\t|Price  |Date\t   |\n|----------------------------------------------------------|\n");
+    while (rc == SQLITE_ROW) {
+        printf("| %d\t|%s\t|%d\t\t|%s\t|%d\t|%s|\n", sqlite3_column_int(stmt, 0),
+               sqlite3_column_text(stmt, 1), sqlite3_column_int(stmt, 2),
+               sqlite3_column_text(stmt, 3), sqlite3_column_int(stmt, 4),
+               sqlite3_column_text(stmt, 5));
+
+        rc = sqlite3_step(stmt);
+    }
+
+    sqlite3_finalize(stmt);
+    sqlite3_close(db);
+
+    return 0;
+}
+
+int getCustomerOrdersByType(int customerId, char* type) {
+    sqlite3* db;
+    
+    int rc = sqlite3_open("Biznis.db", &db);
+    
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Cannot open database: %s", sqlite3_errmsg(db));
+        sqlite3_close(db);
+
+        return 1;
+    }
+
+    char* sql = "SELECT * FROM Orders WHERE CustomerId = ? AND Type = ?;";
+
+    sqlite3_stmt *stmt;
+    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+    
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Failed to prepare statement: %s", sqlite3_errmsg(db));
+
+        return 1;
+    }
+
+    sqlite3_bind_int(stmt, 1, customerId);
+    sqlite3_bind_text(stmt, 2, type, -1, SQLITE_STATIC);
+
+    rc = sqlite3_step(stmt);
+    
+    if (rc != SQLITE_ROW) {
+        fprintf(stderr, "Failed to insert data: %s", sqlite3_errmsg(db));
+
+        return 1;
+    }
+
+    printf("| Id\t|Name\t|Quantity\t|Type\t|Price  |Date\t   |\n|----------------------------------------------------------|\n");
+    while (rc == SQLITE_ROW) {
+        printf("| %d\t|%s\t|%d\t\t|%s\t|%d\t|%s|\n", sqlite3_column_int(stmt, 0),
+               sqlite3_column_text(stmt, 1), sqlite3_column_int(stmt, 2),
+               sqlite3_column_text(stmt, 3), sqlite3_column_int(stmt, 4),
+               sqlite3_column_text(stmt, 5));
+
+        rc = sqlite3_step(stmt);
+    }
+
+    sqlite3_finalize(stmt);
+    sqlite3_close(db);
+
+    return 0;
+}
+
+int main() {
     int choice;
 
     createDB();
 
-    do{
-        printf("1. Add order\n2. Complete order\n3. Alter order\n4. Print balance\n5. Print todo list\n6. Print income\n7. Print expenses\n8. Print cash flow\n9. Exit\n");
+    do {
+        printf("1. Add order\n2. Complete order\n3. Alter order\n4. Print balance\n5. Print todo list\n6. Print income\n7. Print expenses\n8. Print cash flow\n9. Get customer orders by date\n10. Get customer orders by type\n11. Add customer\n12. Delete customer\n13. Exit\n");
         scanf("%d", &choice);
-        switch(choice){
+        switch(choice) {
             case 1:
                 system("clear");
                 char* name, type;
@@ -581,20 +807,21 @@ int main(){
                 printf("Enter quantity of order: ");
                 scanf("%d", &quantity);
                 printf("Enter type of order: ");
-                scanf("%s", &type);
+                scanf(" %c", &type);
                 printf("Enter price of order: ");
                 scanf("%f", &price);
                 printf("Enter date of order in format dd.mm.yyyy: ");
                 scanf("%ms", &date);
-                insertOrder(name, quantity, &type, price, date);
+                int customerId;
+                printf("Enter customer ID: ");
+                scanf("%d", &customerId);
+                insertOrder(name, quantity, &type, price, date, customerId);
                 break;
             case 2:
                 system("clear");
                 int id;
                 printTodo();
-                printf("\n");
-                printf("Enter id of order to complete: ");
-                printf("\n");
+                printf("\nEnter id of order to complete: ");
                 scanf("%d", &id);
                 completeOrder(id);
                 break;
@@ -606,7 +833,7 @@ int main(){
                 printf("\n");
                 scanf("%d", &id2);
                 system("clear");
-                printOrder(id);
+                printOrder(id2);
                 printf("\n");
 
                 alterOrder(id2);
@@ -643,6 +870,51 @@ int main(){
                 break;
             case 9:
                 system("clear");
+                int customerIdByDate;
+                char* dateByDate;
+                printf("Enter customer ID: ");
+                scanf("%d", &customerIdByDate);
+                printf("Enter date in format dd.mm.yyyy: ");
+                scanf("%ms", &dateByDate);
+                getCustomerOrdersByDate(customerIdByDate, dateByDate);
+                getchar();
+                getchar();
+                break;
+            case 10:
+                system("clear");
+                int customerIdByType;
+                char typeByType;
+                printf("Enter customer ID: ");
+                scanf("%d", &customerIdByType);
+                printf("Enter order type (+/-): ");
+                scanf(" %c", &typeByType);
+                getCustomerOrdersByType(customerIdByType, &typeByType);
+                getchar();
+                getchar();
+                break;
+            case 11:
+                system("clear");
+                int newCustomerId;
+                printf("Enter customer ID: ");
+                scanf("%d", &newCustomerId);
+                char* newCustomerName;
+                printf("Enter customer name: ");
+                scanf("%ms", &newCustomerName);
+                addCustomer(newCustomerId, newCustomerName);
+                getchar();
+                getchar();
+                break;
+            case 12:
+                system("clear");
+                int deleteCustomerId;
+                printf("Enter customer ID to delete: ");
+                scanf("%d", &deleteCustomerId);
+                deleteCustomer(deleteCustomerId);
+                getchar();
+                getchar();
+                break;
+            case 13:
+                system("clear");
                 printf("Goodbye!\n");
                 sleep(1);
                 break;
@@ -652,6 +924,6 @@ int main(){
                 break;
         }
         system("clear");
-    }while(choice != 9);
+    } while(choice != 13);
     return 0;
 }
